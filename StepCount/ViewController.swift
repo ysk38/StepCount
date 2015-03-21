67ad4236ad83fb1230313b7bbc3c18b15e4b6c54
ysk38 <yusuke38@gmail.com>
2015-03-21 22:28:31 +0900

    Add steps, distance, floors ascended, floorsdescende.

--- StepCount/ViewController.swift
+++ StepCount/ViewController.swift
@@ -11,10 +11,15 @@ import CoreMotion
 
 class ViewController: UIViewController {
     
-    let altimeter = CMAltimeter()
+    let altimeter:CMAltimeter = CMAltimeter()
+    let pedmeter:CMPedometer = CMPedometer()
     
     @IBOutlet var pressureLabel: UILabel!
     @IBOutlet var altitudeLabel: UILabel!
+    @IBOutlet var steps: UILabel!
+    @IBOutlet var distance: UILabel!
+    @IBOutlet var floorsascended: UILabel!
+    @IBOutlet var floorsdescended: UILabel!
     
     
 
@@ -48,7 +53,38 @@ class ViewController: UIViewController {
         } else {
             println("not use altimeter")
         }
+        
+        let now:NSDate = NSDate();
+        let formatter:NSDateFormatter = NSDateFormatter()
+        formatter.dateFormat = "yyyy-MM-dd"
+        var from:NSDate = self.stringToDate(formatter.stringFromDate(now), isStart: true)
+        
+        if (CMPedometer.isFloorCountingAvailable()) {
+//            pedmeter.queryPedometerDataFromDate(from, toDate: now, withHandler: { (pedometerData:CMPedometerData!, error:NSError!) -> Void in
+//                self.steps.text = String(format: "Steps: %d", pedometerData.numberOfSteps.integerValue)
+//                self.distance.text = String(format: "Distance: %.3f", pedometerData.distance.doubleValue)
+//                self.floorsascended.text = String(format: "FloorsAscended: %d", pedometerData.floorsAscended.integerValue)
+//                self.floorsdescended.text = String(format: "FloorsDescended: %d", pedometerData.floorsDescended.integerValue)
+//            })
+            
+            pedmeter.startPedometerUpdatesFromDate(from, withHandler: { (pedometerData:CMPedometerData!, error:NSError!) -> Void in
+                self.steps.text = String(format: "Steps: %d", pedometerData.numberOfSteps.integerValue)
+                self.distance.text = String(format: "Distance: %.3f", pedometerData.distance.doubleValue)
+                self.floorsascended.text = String(format: "FloorsAscended: %d", pedometerData.floorsAscended.integerValue)
+                self.floorsdescended.text = String(format: "FloorsDescended: %d", pedometerData.floorsDescended.integerValue)
+            })
+            
+        }
+        
+        
     }
 
+    
+    private func stringToDate(date: String, isStart: Bool) -> NSDate {
+        let timestamp = (isStart) ? date + " 00:00:00" : date + " 23:59:59"
+        let formatter:NSDateFormatter = NSDateFormatter()
+        formatter.dateFormat = "yyyy-MM-dd HH:mm:ss"
+        return formatter.dateFromString(timestamp)!
+    }
 }
 
